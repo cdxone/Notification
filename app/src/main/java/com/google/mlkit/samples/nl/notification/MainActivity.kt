d6c2486156b2fb8bd7b701a592364dad7de4f8c2
cdx <937682896@qq.com>
2023-12-05 22:15:41 +0800

    提交代码。

--- app/src/main/java/com/google/mlkit/samples/nl/notification/MainActivity.kt
+++ app/src/main/java/com/google/mlkit/samples/nl/notification/MainActivity.kt
@@ -9,6 +9,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -65,14 +66,6 @@ class MainActivity : AppCompatActivity() {
             }, 5000)
         }
 
-        findViewById<Button>(R.id.btn6).setOnClickListener {
-            val id = CHANNEL_ID + (Math.random() * 100).toInt()
-            Log.i("id", "xxxxid:" + id)
-            createHighChannel(id)
-            findViewById<Button>(R.id.btn6).postDelayed({
-                createHighNormalNoti(id)
-            }, 2000)
-        }
 
         findViewById<Button>(R.id.btn7).setOnClickListener {
             val id = CHANNEL_ID + (Math.random() * 100).toInt()
@@ -80,7 +73,7 @@ class MainActivity : AppCompatActivity() {
             createHighChannel(id)
             findViewById<Button>(R.id.btn6).postDelayed({
                 createHighNormalNoti2(id)
-            }, 2000)
+            }, 1)
         }
 
         findViewById<Button>(R.id.btn8).setOnClickListener {
@@ -120,6 +113,28 @@ class MainActivity : AppCompatActivity() {
         // 展示通知
         val notificationId = CHANNEL_ID.hashCode()
         notificationManager.notify(notificationId, notification)
+
+//        // Get the layouts to use in the custom notification.
+//        val notificationLayout = RemoteViews(packageName, R.layout.notification_small)
+//        val notificationLayoutExpanded = RemoteViews(packageName, R.layout.notification_large)
+//
+//// Apply the layouts to the notification.
+//        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            builder.setChannelId(CHANNEL_ID)
+//        }
+//        val customNotification = NotificationCompat.Builder(context, CHANNEL_ID)
+//            .setSmallIcon(R.drawable.aaa)
+//            .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+//            .setCustomContentView(notificationLayout)
+//            .setCustomBigContentView(notificationLayoutExpanded)
+//            .setCustomContentView(notificationLayout)
+//            .setCustomHeadsUpContentView(notificationLayout)
+//            .build()
+//        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//
+//        notificationManager.notify(666, customNotification)
+
     }
 
     private fun setPriority(builder: NotificationCompat.Builder) {
@@ -179,54 +194,60 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createHighNormalNoti2(id: String) {
-        val intent = Intent(this, TestActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-
-        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.aaa)
-        val notificationBuilder =
-            NotificationCompat.Builder(this, id)
-                .setSmallIcon(R.drawable.aaa)
-                .setContentTitle("HIGH PRIORITY")
-                .setContentText("Check this dog puppy video NOW!")
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setContentIntent(pendingIntent)
-                .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
-                .setCategory(NotificationCompat.CATEGORY_RECOMMENDATION)
-
-        with(NotificationManagerCompat.from(this)) {
-            // notificationId is a unique int for each notification that you must define.
-            if (ActivityCompat.checkSelfPermission(context, "android.permission.POST_NOTIFICATIONS") != PackageManager.PERMISSION_GRANTED) {
-                return
-            }
-            notify((Math.random() * 100).toInt(), notificationBuilder.build())
-        }
-
 //        val intent = Intent(this, TestActivity::class.java).apply {
 //            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
 //        }
-//
 //        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 //
 //        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.aaa)
-//        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
-//            .setSmallIcon(R.drawable.ic_launcher_foreground) //设置小图标
-//            .setContentTitle("ContentTitle")
-//            .setContentText("Much longer text that cannot fit one line...")
-//            .setContentIntent(pendingIntent)
-//            .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
-//            .setPriority(NotificationCompat.PRIORITY_HIGH)
-//            .setCategory(NotificationCompat.CATEGORY_RECOMMENDATION)
-//        // 设置优先级,决定了通知在 Android 7.1 及更低版本上的干扰程度。对于 Android 8.0 及更高版本，请改为设置渠道重要性，
+//        val notificationBuilder =
+//            NotificationCompat.Builder(this, id)
+//                .setSmallIcon(R.drawable.aaa)
+//                .setContentTitle("HIGH PRIORITY")
+//                .setContentText("Check this dog puppy video NOW!")
+//                .setColor(Color.RED)
+//                .setPriority(NotificationCompat.PRIORITY_HIGH)
+//                .setContentIntent(pendingIntent)
+//                .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap).bigLargeIcon(bitmap))
+//                .setCategory(NotificationCompat.CATEGORY_RECOMMENDATION)
 //
 //        with(NotificationManagerCompat.from(this)) {
 //            // notificationId is a unique int for each notification that you must define.
 //            if (ActivityCompat.checkSelfPermission(context, "android.permission.POST_NOTIFICATIONS") != PackageManager.PERMISSION_GRANTED) {
 //                return
 //            }
-//            notify(22222, builder.build())
+//            notify((Math.random() * 100).toInt(), notificationBuilder.build())
 //        }
+
+
+        val intent = Intent(this, TestActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.aaa)
+        var builder = NotificationCompat.Builder(this, id)
+            .setSmallIcon(R.drawable.ic_launcher_foreground) //设置小图标
+            .setLargeIcon(bitmap)
+            .setContentTitle("ContentTitle")
+            .setTicker("welcome")
+            .setContentText("Much longer text that cannot fit one line...")
+            .setContentIntent(pendingIntent)
+            .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
+            .setOngoing(true)
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_RECOMMENDATION)
+        // 设置优先级,决定了通知在 Android 7.1 及更低版本上的干扰程度。对于 Android 8.0 及更高版本，请改为设置渠道重要性，
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define.
+            if (ActivityCompat.checkSelfPermission(context, "android.permission.POST_NOTIFICATIONS") != PackageManager.PERMISSION_GRANTED) {
+                return
+            }
+            notify(22222, builder.build())
+        }
     }
 
     private fun createInboxNormalNoti() {
@@ -294,10 +315,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createNormalNoti() {
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
         var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
             .setSmallIcon(R.drawable.ic_launcher_foreground) //设置小图标
             .setContentTitle("ContentTitle")
             .setContentText("ContentText")
+            .setFullScreenIntent(pendingIntent, false)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT) // 设置优先级
         // 设置优先级,决定了通知在 Android 7.1 及更低版本上的干扰程度。对于 Android 8.0 及更高版本，请改为设置渠道重要性，
 
